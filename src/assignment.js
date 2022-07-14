2a02c64c3d1478bb7c10c19e2c44093c2132204b
Udofa Daniel <danieludofa452@gmail.com>
2022-07-14 20:04:35 +0000

    Passed 1

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers) { 
+  let sum = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+  sum += arrayOfNumbers[i];
+  }
+  return sum;
+    
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+  for(let i = 0; i < arrayOfNumbers.length; i++){
+    if(i === 0 || i % 2 != 0){
+     count;
+      }else{
+      count+=1;
+      }
+    }
+return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
